3850e7a35e859903768ccb62d1ae60522d5c4a93
Asma Hassan <asmasheikhhassan@gmail.com>
2023-06-02 19:40:15 +0100

    Edited cart

--- e-commerce-website/src/components/pages/Cart.js
+++ e-commerce-website/src/components/pages/Cart.js
@@ -1,8 +1,5 @@
-// import { Add, Remove } from "@material-ui/icons";
+
 import styled from "styled-components";
-// import Announcement from "../components/Announcement";
-// import Footer from "../components/Footer";
-// import Navbar from "../components/NavBar";
 import Add from '@mui/icons-material/AddSharp';
 import Remove from '@mui/icons-material/RemoveSharp';
 import { mobile } from "./responsive";
