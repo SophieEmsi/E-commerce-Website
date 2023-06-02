311c93231d35df55c229497128726312c128267c
Sophie McNally <soph_mcnally@msn.com>
2023-06-02 17:58:19 +0100

    adding initial styling to results page

--- e-commerce-website/src/components/pages/Results.js
+++ e-commerce-website/src/components/pages/Results.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useParams } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
 
 const Results = () => {
   const [results, setResults] = useState([]);
@@ -19,12 +20,18 @@ const Results = () => {
       <Row>
         <Col>
           <h1>Results</h1>
-          {results.map(result => (
-            <div key={result.id}>
-              <h3>{result.title}</h3>
-              <p>{result.price}</p>
-            </div>
-          ))}
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {results.map(result => (
+              <Card key={result.id} className="shadow-lg">
+                <Card.Img variant="top" src={result.image} />
+                <Card.Body>
+                  <Card.Title>{result.title}</Card.Title>
+                  <Card.Text>{result.price}</Card.Text>
+                  <Button variant="primary">Add to Cart</Button>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
         </Col>
       </Row>
     </Container>
@@ -32,5 +39,6 @@ const Results = () => {
 };
 
 export default Results;
+
 
 
