621c9eeb681182d54dcf20e5af6191d13afdb8f0
Sophie McNally <soph_mcnally@msn.com>
2023-06-03 21:22:10 +0100

    updating collection titles

--- e-commerce-website/src/components/Collections.js
+++ e-commerce-website/src/components/Collections.js
@@ -3,29 +3,29 @@ import React from 'react';
 
 const callouts = [
     {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
+      name: 'Womens Clothing',
+      description: 'Timeless Fashion for Women',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
       imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
       href: '#',
     },
     {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
+      name: 'Mens Clothing',
+      description: 'Stylish Apparel for Men',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
       imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
       href: '#',
     },
     {
-      name: 'Travel',
-      description: 'Daily commute essentials',
+      name: 'Jewellery',
+      description: 'Elegant Accessories',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
       imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
       href: '#',
     },
         {
-      name: 'Travel',
-      description: 'Daily commute essentials',
+      name: 'Electronics',
+      description: 'Cutting-Edge Gadgets',
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
       imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
       href: '#',
