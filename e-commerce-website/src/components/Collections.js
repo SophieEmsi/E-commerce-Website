63bc37bc1c37b81deb8e4f1dc0a122f079c203d1
Asma Hassan <asmasheikhhassan@gmail.com>
2023-06-06 05:44:44 +0100

    Edited the hover effect on the collections page - so now when you hover the images the card name and desc. appears and when you click it takes you that page

--- e-commerce-website/src/components/Collections.js
+++ e-commerce-website/src/components/Collections.js
@@ -28,56 +28,80 @@ const callouts = [
   },
 ];
 
-export default function Collections() {
-  const [data, setData] = useState(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://fakestoreapi.com/products');
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+  
+  export default function Collections() {
+    const [data, setData] = useState(null);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('https://fakestoreapi.com/products');
+          const jsonData = await response.json();
+          setData(jsonData);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    const handleCategoryClick = (category) => {
+      navigate(`/results/${category}`);
     };
-
-    fetchData();
-  }, []);
-
-  const handleCategoryClick = (category) => {
-    navigate(`/results/${category}`);
-  };
-
-  return (
+  
+    const handleMouseEnter = (index) => {
+      setHoveredIndex(index);
+    };
+  
+    const handleMouseLeave = () => {
+      setHoveredIndex(null);
+    };
+  
+    return (
     <div className="bg-white mb-3">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-5 sm:py-24 lg:max-w-none lg:py-32">
           <h2 className="text-3xl font-bold text-center mb-5 text-gray-900">Collections</h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-4 lg:gap-x-6 lg:space-y-0">
-            {data && callouts.map((callout, index) => (
-              <div key={index} className="group relative">
+            {data &&
+              callouts.map((callout, index) => (
                 <div
-                  className="relative overflow-hidden rounded-lg bg-white"
-                  style={{ height: '400px', width: '100%' }}
+                  key={index}
+                  className="drop-shadow-2xl group relative"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
                 >
-                  <img
-                    src={callout.imageSrc}
-                    alt={callout.name}
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-                <h3 className="mt-6 text-lg text-gray-500">
-                  <Link to={`/results/${callout.category}`} onClick={() => handleCategoryClick(callout.category)}>
-                    <span className="absolute inset-0" />
-                    {callout.name}
+                  <Link
+                    to={`/results/${callout.category}`}
+                    onClick={() => handleCategoryClick(callout.category)}
+                  >
+                    <div
+                      className="relative overflow-hidden rounded-lg bg-white"
+                      style={{ height: '400px', width: '100%' }}
+                    >
+                      <img
+                        src={callout.imageSrc}
+                        alt={callout.name}
+                        className="h-full w-full object-cover object-center"
+                      />
+                      {hoveredIndex === index && (
+                        <div className="absolute inset-0 flex flex-col items-center justify-center">
+                          <h3 className="text-center text-lg text-black bg-white p-2 font-medium">
+                            {callout.name}
+                          </h3>
+                          <p className="text-lg text-black bg-white p-2 mt-2 font-medium">
+                            {callout.description}
+                          </p>
+                        </div>
+                      )}
+                    </div>
                   </Link>
-                </h3>
-                <p className="text-base font-semibold text-lg text-gray-900">{callout.description}</p>
-              </div>
-            ))}
+                </div>
+              ))}
           </div>
         </div>
       </div>
