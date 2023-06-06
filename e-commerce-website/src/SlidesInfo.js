8d630f65fc79ffdded8885fa6fbd1238823809b1
Asma Hassan <asmasheikhhassan@gmail.com>
2023-06-06 05:58:16 +0100

    Changed the order of the slides

--- e-commerce-website/src/SlidesInfo.js
+++ e-commerce-website/src/SlidesInfo.js
@@ -1,17 +1,18 @@
 export const sliderItems = [
     {
-      id: 1,
-      img: "https://i.postimg.cc/J0ZkqKfJ/bg.png",
-      title: "SUMMER SALE",
-      desc: "EMBRACE STYLE WITHOUT COMPROMISE - 30% OFF NEW ARRIVALS!",
-      // bg: "F8FAE8"
-    },
-    {
-      id: 2,    
+      id: 1,    
       img: "https://i.postimg.cc/MGBqX4Vy/bg2.png",
       title: "SUMMER COLLECTION",
       desc: "DISCOVER THE BEAUTY OF OUR STUNNING SUMMER COLLECTION.",
       // bg: "F8FAE8",
+
+    },
+    {
+      id: 2,
+      img: "https://i.postimg.cc/J0ZkqKfJ/bg.png",
+      title: "SUMMER SALE",
+      desc: "EMBRACE STYLE WITHOUT COMPROMISE - 30% OFF NEW ARRIVALS!",
+      // bg: "F8FAE8"
     },
     {
       id: 3,
