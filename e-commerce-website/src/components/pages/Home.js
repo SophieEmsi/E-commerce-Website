407c227b136bf2251ca7f3039ef90adbbce93bbe
Asma Hassan <asmasheikhhassan@gmail.com>
2023-06-08 05:01:57 +0100

    Removed unused functions

--- e-commerce-website/src/components/pages/Home.js
+++ e-commerce-website/src/components/pages/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Slides from '../Slides';
-// import Announcement from '../Announcement';
-// import NavBar from '../NavBar';
-// import Cart from './Cart';
 import Stats from '../Stats';
 import Collections from '../Collections';
 import Banner from '../Banner';
